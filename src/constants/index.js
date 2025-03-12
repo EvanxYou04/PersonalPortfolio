45514a88efa31d3489493ced082b1f5d84156d77
Evan <evanyou2004@gmail.com>
2025-03-11 20:00:08 -0500

    add required files

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/evanxyou/",
+  },
+  {
+    id: "social-media-2",
+    icon: github,
+    link: "https://github.com/EvanxYou04/",
+  },
+];
